ef21b835402c9ca4522ad3bbad5433a88dcd860f
Phuc Tran Huu <phuc.tranh@one-line.com>
2022-03-20 15:40:51 +0700

    [Android] Practice with find element by class name on android

--- test/specs/android-findElements.spec.js
+++ test/specs/android-findElements.spec.js
@@ -1,4 +1,7 @@
 describe('Android Elements Tests',()=>{
+    /// Acessibility ID:
+    ///  - Crossplatform compatibility
+    ///  - Preferred option
     it('Find element by accessibility Id', async () => {
         // find element by accessibility Id
         const appOption = await $('~App');
@@ -10,4 +13,20 @@ describe('Android Elements Tests',()=>{
         const actionBar = await $('~Action Bar');
         await expect(actionBar).toBeExisting();
     });
+
+    /// Class Name/Tag Name:
+    ///  - Not Unique(usually)
+    ///  - Ex: TextView, Button, Layout
+    /// it.only() is Indicates this test should be executed exclusively. 
+    /// - Chỉ chạy 1 mình nó. Không chạy những test khác
+    it.only('Find element by class name/tag name', async () => {
+        // find element by class name
+        const className = $('android.widget.TextView');
+
+        console.log(await className.getText());
+
+        // Assertion
+        await expect(className).toHaveText('API Demos');
+    });
 });
+
